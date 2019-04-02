1811e60a93ac13d3795b6490c9a4fee60c1b498e
denestoth <denes.toth@outlook.com>
2019-04-02 23:27:53 +0200

    add project skeleton

---
+++ src/main/java/com/dnstth/bdcg/BdcgApplication.java
@@ -0,0 +1,13 @@
+package com.dnstth.bdcg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BdcgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BdcgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dnstth/bdcg/BdcgApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dnstth.bdcg;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BdcgApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
