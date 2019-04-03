c6bf7b1798f2b534a36d6e3e768f01945f6c356a
denestoth <denes.toth@outlook.com>
2019-04-03 18:48:11 +0200

    add cardhelper

--- src/main/java/com/dnstth/bdcg/helper/CardHelper.java
+++ src/main/java/com/dnstth/bdcg/helper/CardHelper.java
@@ -1,4 +1,13 @@
 package com.dnstth.bdcg.helper;
 
+import com.dnstth.bdcg.model.Card;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import java.util.List;
+
 public class CardHelper {
+
+    public static List<Card> shuffle(final List<Card> cards) {
+        throw new NotImplementedException();
+    }
 }
