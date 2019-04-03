533ab0118c59dee145780c58f9443865c9f21b1f
denestoth <denes.toth@outlook.com>
2019-04-03 17:10:46 +0200

    code formatting

--- src/test/java/com/dnstth/bdcg/BdcgApplicationTests.java
+++ src/test/java/com/dnstth/bdcg/BdcgApplicationTests.java
@@ -9,8 +9,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class BdcgApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
